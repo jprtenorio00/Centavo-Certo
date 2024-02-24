91a453dedf4f181cc834b6681d216524c7d9339a
jprtenorio00 <jp-tenorio@hotmail.com>
2024-02-24 18:41:27 -0300

    Ajustes header component e seção principal do start component

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,52 +1,55 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, SimpleChanges } from '@angular/core';
 import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { AuthService } from 'src/app/service/auth.service';
 import { LoginService } from 'src/app/service/login.service';
-
-interface UserData {
-  name: string;
-  plan: string;
-}
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit, OnDestroy {
+export class HeaderComponent implements OnInit {
 
-  isLoggedIn = false;
-  private loginStatusSub: Subscription;
-  private userSub: Subscription;
-  userName = '';
-  userPlan = '';
-  userData: UserData = { name: '', plan: '' };
+  isLoggedIn: boolean = false;
+  private subscription: Subscription = new Subscription();
+  userName: string = '';
+  userPlan: string = '';
+  userData: any;
 
-  constructor(private loginService: LoginService, private authService: AuthService, private router: Router) { }
+  constructor(private loginService: LoginService, private authService: AuthService, private router: Router,) { }
 
   ngOnInit() {
-    this.loginStatusSub = this.authService.isUserLoggedIn().subscribe(isLoggedIn => {
+    this.observableStatusLogin();
+  }
+
+  observableStatusLogin(){
+    this.subscription = this.authService.isUserLoggedIn().subscribe(isLoggedIn => {
       this.isLoggedIn = isLoggedIn;
-      if (isLoggedIn) {
-        this.userSub = this.authService.getUserLoggedIn().subscribe(
-          data => {
-            this.userData = data;
-            this.userName = data.name;
-            this.userPlan = data.plan;
-          }
-        );
-      }
+      this.authService.formGroup.get('isLoggedIn')?.patchValue(isLoggedIn)
+      isLoggedIn ? this.fetchUserLoggedIn() : ''
     });
   }
 
   ngOnDestroy() {
-    this.loginStatusSub?.unsubscribe();
-    this.userSub?.unsubscribe();
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
   }
 
-  scrollToTop() {
-    window?.scrollTo({ top: 0, behavior: 'smooth' });
+  async fetchUserLoggedIn(){
+    this.userData = await this.authService.getUserLoggedIn();
+    if(this.userData)
+    {
+      this.userName = this.userData?.name;
+      this.userPlan = this.userData?.plan
+    }
+  }
+
+  scrollToTop(){
+    if (typeof window !== 'undefined') {
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+    }
   }
 
   onLogout() {
