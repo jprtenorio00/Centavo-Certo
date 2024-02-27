fed1d38113c22aa67aba15fd81ea9d167cba2ce7
jprtenorio00 <jp-tenorio@hotmail.com>
2024-02-27 20:46:35 -0300

    Commit backup

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -7,7 +7,7 @@ import { LoginService } from 'src/app/service/login.service';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
 
